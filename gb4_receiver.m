9fcfa3965fe47772c3abc4817bc3664fb9729952
Matias Silva <git@matiasilva.com>
2022-05-27 16:22:23 +0100

    Update gb4_receiver.m

--- gb4_receiver.m
+++ gb4_receiver.m
@@ -34,15 +34,49 @@ function calibration(a)
     fprintf('average noise: %.2fV, std of noise: %.3fV\n', u_noise, std_noise);
 
     disp('send three ones')
-    detect(a, u_noise, std_noise);
+    pulses = zeros(3);
+    for i = 1:3
+        pulse = detect(a, u_noise, std_noise);
+        pulses(i) = pulse;
+    end
+    [u_peak, std_peak, u_width, std_width] = postProcess(pulses);
+    fprintf('average peak of 1s: %.2fV, std of peak: %.3fV\n', u_peak, std_peak);
+    fprintf('average width of 1s from 0.9% of u_peak: %.2fV, std of width: %.3fV\n', u_width, std_width);
 
     disp('send three zeros')
+    pulses = zeros(3);
+    for i = 1:3
+        pulse = detect(a, u_noise, std_noise);
+        pulses(i) = pulse;
+    end
+    [u_peak, std_peak, u_width, std_width] = postProcess(pulses);
+    fprintf('average peak of 0s: %.2fV, std of peak: %.3fV\n', u_peak, std_peak);
+    fprintf('average width of 0s from 0.9% of u_peak: %.2fV, std of width: %.3fV\n', u_width, std_width);
 end
 
-function [u_peak, std_peak, u_width, std_width] = postProcess(pulse, dvsthresh)
+function [u_peak, std_peak, u_width, std_width] = postProcess(pulses, dvsthresh)
+    us_peak = [];
+    us_width = [];
+    for j = 1:3
+         pulse = pulses(j);
+         [m, i] = max(pulse);
+         roll_avg = movmean(pulse, 8);
+         us_peak = [us_peak, roll_avg(i)]
+         thresh = 0.9 * roll_avg(i);
+         % left half
+         [~,~,k_min]=unique(round(abs(pulse(1:i)-thresh)),'stable');
+         % right half
+         [~,~,k_max]=unique(round(abs(pulse(i:end)-thresh)),'stable');
+         disp(k_max, k_min);
+         us_width = [us_width, k_max - k_min];
+    end
+    u_peak = mean(us_peak);
+    std_peak = std(us_peak);
+    u_width = mean(us_width);
+    std_width = std(us_width);
 end
 
-function detect(a, u_noise, std_noise)
+function pulse = detect(a, u_noise, std_noise)
     isDetecting = true;
     isFalling = false;
     isTrackingFall = true;
@@ -63,14 +97,19 @@ function detect(a, u_noise, std_noise)
         roll_avg_pulse = movmean(pulse, 3);
         
         % ignore rises after first fall
-        isFalling = roll_avg_dvs(end) < 0;
+        if isTrackingFall
+            isFalling = roll_avg_dvs(end) < 0;
+        end
         % ignore any signal below 0.5std of noise
         isAboveNoise = roll_avg_pulse(end) > n_thresh;
+
+        % start condition for fall detection
         if isFalling && isAboveNoise && isTrackingFall
             disp('locked onto fall');
             isTrackingFall = false;
         end
 
+        % loop exit condition
         if isFalling && ~isAboveNoise && ~isTrackingFall
             disp('peak finished')
             isDetecting = false;
