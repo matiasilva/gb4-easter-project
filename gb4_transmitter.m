7dd20abda12f8a7983ce4d637488298306f362ac
Matias Silva <git@matiasilva.com>
2022-05-31 10:57:51 +0100

    Update gb4_transmitter.m

--- gb4_transmitter.m
+++ gb4_transmitter.m
@@ -6,32 +6,35 @@ function main()
     pT = timer("StartDelay", 0.05);
     pT.TimerFcn = @(~,~)fprintf('');
     shortT = timer("StartDelay", 0.1);
-    shortT.TimerFcn = @(~,~)disp('sent a 0, waiting..');
+    shortT.TimerFcn = @(~,~)disp('sent a 0');
     longT = timer("StartDelay", 0.4);
-    longT.TimerFcn = @(~,~)disp('sent a 1, waiting..');
+    longT.TimerFcn = @(~,~)disp('sent a 1');
     iT = timer("StartDelay", 10);
     iT.TimerFcn = @(~,~)fprintf('');
 
     setRelay(a, 0);
 
     % main loop
-    while true
-        bit = input('enter desired binary output\n');
-        modulation(bit, a, pT, shortT, longT, iT);
+    n = 10; % bit count
+    bitsToSend = randi(2,n,1) - 1;
+    for i = 1:n
+        %bit = input('enter desired binary output');
+        bit = bitsToSend(i);
+        modulation(bit, pT, longT, shortT, iT, a);
     end
 end
 
-function modulation(bit, a, pT, shortT, longT, iT)
-    if bit == 0
-        doSpray(a, pT, shortT, iT);
-    elseif bit == 1
-        doSpray(a, pT, longT, iT);
+function modulation(bit, pT, longT, shortT, iT, a)
+    if bit == '0'
+        doSpray(pT, shortT, iT, a);
+    elseif bit == '1'
+        doSpray(pT, longT, iT, a);
     else
         fprintf('invalid input\n');
     end
 end
 
-function doSpray(a, pulseT, sprayT, intervalT)
+function doSpray(pulseT, sprayT, intervalT)
     doPulse(pulseT, a);
     start(sprayT);
     wait(sprayT);
